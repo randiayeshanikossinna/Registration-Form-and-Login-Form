c0c263a3a35bf187bae2d35f2cdcc8ef0fead2a1
randiayeshanikossinna <randiayeshani@gmail.com>
2020-11-27 20:06:11 -0800

    Registration part was started.

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+      <title>Login & Registration Form</title>
+      <link rel="stylesheet"  href="css/style.css">
+ </head>
+ <body>
+     <div class="header">
+     <h2>Register</h2>
+     </div>
+ </body>
+      
+</html>
