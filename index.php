d305fe308d201a65cd4b3c659bb5572564ee51d9
randiayeshanikossinna <randiayeshani@gmail.com>
2020-11-30 17:13:14 -0800

    Registration form was created

--- index.php
+++ index.php
@@ -8,8 +8,13 @@
      <h1>Register Form </h1>
      </div>
     <div class="inputdata">
-           <form action="">
-           <label><input type="text"></label>
+           <form action="register.php" method="form">
+           <label>User Name<input type="text" name="uname"></label> <br> <br>
+           <label>Email<input type="text" name="email"></label> <br><br>
+           <label>Password<input type="password" name="password_1"></label> <br> <br>
+           <label>Comform Password<input type="password" name="password_2"></label> <br><br>
+          <button type="submit" Name="register" class="btn">Register</button>
+
            </form>
     </div>
  </body>
