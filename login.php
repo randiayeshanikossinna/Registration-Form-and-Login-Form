707aafae2d5dd01dfcc1406e1df5725f9c420e25
randiayeshanikossinna <randiayeshani@gmail.com>
2020-11-30 18:46:32 -0800

    Login page also created.

--- login.php
+++ login.php
@@ -6,19 +6,17 @@
  <body>
 
      <div class="header">
-     <h1>Register Form </h1>
+     <h1>Login Form </h1>
      </div>
 
     <div class="inputdata">
   
     <form action="register.php" method="form">
            <label>User Name<input type="text" name="uname"></label> <br> <br>
-           <label>Email<input type="text" name="email"></label> <br><br>
            <label>Password<input type="password" name="password_1"></label> <br> <br>
-           <label>Comform Password<input type="password" name="password_2"></label> <br><br>
-          <button type="submit" Name="register" class="btn">Register</button>
+          <button type="submit" Name="register" class="btn">Login</button>
           <p>
-           Already a member ? <a href="login.php">Sign in</a>
+           Not yet a member ? <a href="index.php">Sign Up</a>
           </p>
            </form>
            </div>
